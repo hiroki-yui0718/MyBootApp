c6d7e61b202233c91b8deb017282d05c435d1518
広記 由井 <173224@ocsjoho.onmicrosoft.com>
2019-11-14 08:24:45 +0900

    DataBase Null Shori OK!

--- src/main/java/com/tuyano/springboot/controller/CalendarController.java
+++ src/main/java/com/tuyano/springboot/controller/CalendarController.java
@@ -1,36 +1,38 @@
 package com.tuyano.springboot.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
+import javax.persistence.NoResultException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.tuyano.springboot.model.Account;
 import com.tuyano.springboot.service.CalendarService;
 
 @Controller
 public class CalendarController {
-	
+
 	@Autowired
 	CalendarService service;
-	
+
 	String[] week = {" ","日","月","火","水","木","金","土"};
 	private int startDay;
 	private int lastDate;
+	private int year;
 	private int month;
-	
+
 	public void setCal() {
 		Calendar cal = Calendar.getInstance();
-		int year = cal.get(Calendar.YEAR);
+		this.year = cal.get(Calendar.YEAR);
 		this.month  = cal.get(Calendar.MONTH) + 1;
 		cal.clear();
 		// 月の初めの曜日を求めます。
@@ -40,7 +42,7 @@ public class CalendarController {
 		cal.add(Calendar.MONTH, 1);
 		cal.add(Calendar.DATE, -1);
 		this.lastDate = cal.get(Calendar.DATE);
-		
+
 	}
 
 	public List<String> week(){
@@ -48,7 +50,7 @@ public class CalendarController {
 		setCal();
 		int i = 1;
 		int j = startDay;
-		
+
 		loop: while(true) {
 			while(j < week.length) {
 				list.add(week[j]);
@@ -72,17 +74,28 @@ public class CalendarController {
 		}
 		return list;
 	}
-	
+
 	@RequestMapping("/calendar")
 	public ModelAndView index(ModelAndView mav,Authentication authentication) {
 		User userDetail = (User)authentication.getPrincipal();
-        String name = userDetail.getUsername();
-        System.out.println(name);
-//		service.findSumTime();
+		String name = userDetail.getUsername();
+		setCal();
+
+		List<LocalTime> list = new ArrayList<>();
+		for(int i = 1;i <= lastDate;i++) {
+			try{
+				LocalDateTime t1 = LocalDateTime.of(year,month,i,0,0,0);
+				LocalDateTime t2= LocalDateTime.of(year,month,i,23,59,59);
+				list.add(service.findSumTime(name,t1,t2));
+			}catch(NoResultException e) {
+				list.add(null);
+			}
+
+		}
 		mav.setViewName("calendar");
 		mav.addObject("day",day());
 		mav.addObject("week",week());
-//		mav.addObject("SumTIme")
+		mav.addObject("sumTime",list);
 		return mav;
 	}
 }
